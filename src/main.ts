2bd14aeca190d84101ba4d7ae99bda77694361dc
Md Sayedul Haque <takisayedul@gmail.com>
2025-11-17 23:01:27 +0600

    Add: update table name

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   // optional: enable CORS (if using frontend)
   app.enableCors();
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3344;
   await app.listen(port);
   console.log(`🚀 Server running on http://localhost:${port}`);
 }
--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,6 +1,6 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
+@Entity('users')
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
